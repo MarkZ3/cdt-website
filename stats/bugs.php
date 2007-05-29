bbc87eb8e5825f2b1770d282eb910ccdebb9186b
dschaefer <dschaefer>
2007-05-29 17:08:54 +0000

    New bugs report page.

--- stats/bugs.php
+++ stats/bugs.php
@@ -7,8 +7,37 @@
 		$dbc = new DBConnectionBugs();
 		$dbh = $dbc->connect();
 		
+		$sql = "select
+					bug.bug_id,
+					bug.short_desc,
+					usr.realname as somedude
+				from
+					bugs as bug
+					inner join profiles as usr on usr.userid = bug.reporter
+				where
+					bug.bug_id = 12345";
+		
+		$rs = mysql_query($sql, $dbh);
+		
+		if (mysql_errno($dbh) > 0) {
+			echo "There was an error processing this request ";
+			echo mysql_error($dbh);
+			$dbc->disconnect();
+			exit;
+		}
+		
+		while($myrow = mysql_fetch_assoc($rs)) {
+			echo "Bug ID: " . $myrow['bug_id'] . " Description: " . $myrow['short_desc'] . " Reporter: " . $myrow['somedude'];
+		}
+		
+		$dbc->disconnect();
+	
+		$rs 		= null;
+		$dbh 		= null;
+		$dbc 		= null;
+
 	} else {
-		echo "Not authorized (2)";
+		echo "Not authorized (3)";
 	}
 	
 ?>

56de9703654dcda29d391c6341817385aa608d39
dschaefer <dschaefer>
2008-04-01 18:39:14 +0000

    Patch for 224517 from Mike.

--- index.php
+++ index.php
@@ -1,22 +1,9 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 
-	#*****************************************************************************
-	#
-	# index.php
-	#
-	# Author: 		Doug Schaefer
-	# Date:			2006-07-01
-	#
-	# Description:  CDT main page
-	#
-	#
-	#****************************************************************************
-	
-	#
 	# Begin: page-specific settings.  Change these. 
 	$pageTitle 		= "Eclipse C/C++ Development Tooling - CDT";
 	$pageKeywords	= "CDT, Eclipse, C++, IDE, C++ IDE";
-	$pageAuthor		= "Doug Schaefer";
+	$pageAuthor		= "CDT Community";
 	
 	# Add page-specific Nav bars here
 	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
@@ -25,47 +12,148 @@
 	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
 
 	# End: page-specific settings
-	#
 		
 	# Paste your HTML content between the EOHTML markers!	
 	$html = <<<EOHTML
 	
+	
+<div id="container">
 <div id="maincontent">
-	<div id="midcolumn">
-		<h1>$pageTitle</h1>
-		<h2>Welcome to the Eclipse CDT</h2>
-		
-		<p><b>CDT 4.0.3 Now Available</b> - February 26, 2008
-		- As part of the Europa Winter Maintenance release, CDT 4.0.3 includes numerous fixes to CDT 4.0.2.  To download, please visit our Downloads page on the left.</p>
-		
-		<p><b>CDT 4.0.2 Now Available</b> - November 30, 2007
-		- CDT 4.0.2 includes numerous fixes to CDT 4.0.1.  To download, please visit our Downloads page on the left.</p>
-		
-		<p><b>CDT 4.0.1 Now Available</b> - September 26, 2007
-		- CDT 4.0.1 includes numerous fixes to CDT 4.0.</p>
-		
-		<p><b>CDT 4.0 Now Available</b> - June 29, 2007
-		- CDT 4.0 is our biggest and best release to date. Check out our 
-		<a href="http://help.eclipse.org/help33/index.jsp?topic=/org.eclipse.cdt.doc.user/concepts/cdt_c_whatsnew.htm">
-		New and Noteworthy</a> page in the on-line C/C++ Development User Guide.
-		For download instructions, check out our Downloads page by clicking the link on the left.</p>
-			
-		<p>Obviously this, the main CDT web site, is sorely lacking in content.
-		Until we get time to work on this site, the latest information about the CDT project can be found on the
-		<a href="http://wiki.eclipse.org/index.php/CDT">CDT Wiki</a>. As well the old page is still available
-		<a href="index_old.html">here</a>, but it is now getting really old.</p>
+
+<div id="midcolumn">
+	<h1>Eclipse C/C++ Development Tooling - CDT</h1><br>
+	<div id="LeftImage"><img src="images/snapshots.gif" alt="" height="366" width="269" border="0"><br>
+		<!--Content regarding image-->
 	</div>
-	<div id="rightcolumn">
-		<div class="sideitem">
-			<h6>Related Links</h6>
-			<ul><li><a href="http://www.eclipse.org/newsportal/thread.php?group=eclipse.tools.cdt">CDT newsgroup</a> (<a href="http://www.eclipse.org/newsgroups/register.php">register</a>)</li>
-			<li><a href="http://wiki.eclipse.org/index.php/CDT">Wiki</a></li>
-			<li><a href="https://dev.eclipse.org/mailman/listinfo/cdt-dev">Mailing list</a></li>
-			<li><a href="/projects/project_summary.php?projectid=tools.cdt">Project Information</a></li>
-			<li><a href="/downloads">Download C++ IDE</a></li></ul>
-		</div>
+	<h2>Welcome to the Eclipse CDT Project</h2>
+	<p>The CDT (C/C++ Development Tools) Project provides a fully functional C and C++ Integrated Development Environment (IDE) for the Eclipse platform.</p>
+
+	<br>
+	<h2>News</h2>
+
+	<p><b>CDT 4.0.3 Now Available</b> - February 26, 2008<br>As part of the Europa Winter Maintenance release, CDT 4.0.3 includes numerous fixes to CDT 4.0.2.</p>
+	<p><b>CDT 4.0.2 Now Available</b> - November 30, 2007<br>Includes numerous fixes to CDT 4.0.1.</p>
+	<p><b>CDT 4.0.1 Now Available</b> - September 26, 2007<br>Includes numerous fixes to CDT 4.0.</p>
+	<p><b>CDT 4.0 Now Available</b> - June 29, 2007<br>Our biggest and best release to date. Check out our <a href="http://help.eclipse.org/help33/index.jsp?topic=/org.eclipse.cdt.doc.user/concepts/cdt_c_whatsnew.htm">New and Noteworthy</a> page in the on-line C/C++ Development User Guide.</p>
+
+	<hr class="clearer" />
+
+	<div class="homeitem">
+		<h3>Articles</h3>
+			<ul>
+
+
+			  <li>
+				<a href="http://www.ibm.com/developerworks/opensource/library/os-eclipse-stlcdt/">
+				Developing applications using the Eclipse C/C++ Development Toolkit </a><br>
+				IBM developerWorks article.
+			  </li>
+			  <li>
+				<a href=" http://www.ibm.com/developerworks/opensource/library/os-ecc/?S_TACT=105AGX44&S_CMP=ART">
+				C/C++ development with the Eclipse Platform</a><br>
+				IBM developerWorks article.
+			  </li>
+			  <li>
+				<a href="http://www-128.ibm.com/developerworks/library/os-ecl-vscdt/index.html?ca=dgr-eclipse-1">
+				Migrate Visual Studio C and C++ projects to Eclipse CDT
+				</a><br>
+				A step-by-step guide to moving Microsoft VS projects to Eclipse C/C++ Development Toolkit.
+			  </li>
+			  <li>
+				<a href="http://forge.mysql.com/wiki/Eclipse/CDT_on_Linux_and_Mac_OS_X">
+				Eclipse/CDT on Linux and Mac OS X </a><br>
+				Covers installation, configuration, and basic debugging of Eclipse/CDT Europa edition using MySQL as source code.
+			  </li>
+			  <li>
+				<a href="http://www.infoq.com/articles/aspectj-with-ajdt">
+				Building a CDT-based editor
+				</a><br>
+				Five part article series on IBM developerWorks.
+			  </li>
+			  <li>
+				<a href="http://www.codeproject.com/KB/tips/CPP_Dev_eclipse_CDT.aspx">
+				C++ Development using eclipse IDE
+				</a><br>
+				Starters guide.
+			  </li>
+
+			</ul>
+
+	</div>
+
+	<div class="homeitem">
+		<h3>Resources</h3>
+			<ul>
+		      <li>
+				<a href="http://live.eclipse.org/node/293">
+				CDT 4.0 Webinar</a><br>
+				This webinar will walk through all of CDT’s features from new project creation, code editing, and source navigation, to build and debug with a special focus on what’s new in CDT 4.0.
+			  </li>
+			  <li>
+				<a href="http://live.eclipse.org/node/197">
+				Developing C/C++ Applications Webinar</a>
+			  </li>
+			  <li>
+				<a href="http://wiki.eclipse.org/CDT/User/FAQ">
+				CDT FAQ</a><br>
+				All things CDT. May be slightly out of date.
+			  </li>
+
+			  <li>
+				<a href="http://cdt-devel-faq.wikidot.com/">
+				Unofficial CDT Developers FAQ</a>
+			  </li>
+			  <li>
+				<a href="http://www.eclipse.org/cdt/index_old.html">
+				Old CDT Website</a><br>
+				Covers CDT 3.0 and earlier.
+			  </li>
+			  <li>
+				<a href="http://wascana.sourceforge.net/">
+				Wascana Desktop Developer</a><br>
+				CDT based distribution for Windows development.
+			  </li>
+			  <li>
+				<a href="http://cdtdoug.blogspot.com/index.html">
+				Doug on the Eclipse CDT</a><br>
+				All the latest news from the CDT world.
+			  </li>
+
+
+
+			</ul>
+	</div>
+
+</div>
+<div id="rightcolumn">
+	<div id="DownloadR">
+
+		<a href="http://www.eclipse.org/downloads">
+			<img src="images/download.jpg" alt="" height="133" width="143" border="0">
+			<br>
+			Download CDT
+		</a>
+
+	</div>
+	<div id="DL_leftNavR">
+		<a href="http://www.eclipse.org/newsportal/thread.php?group=eclipse.tools.cdt">CDT newsgroup</a><br>
+		<img src="images/RegisterArrow.gif" alt="" height="23" width="31" align="absmiddle" border="0">
+		<a href="http://www.eclipse.org/newsgroups/register.php">Register</a><br>
+		<img src="images/subNav_bars.gif" alt="" height="8" width="100%" border="0" vspace="5"><br>
+		<a href="http://wiki.eclipse.org/index.php/CDT">Wiki </a><br>
+		<img src="images/subNav_bars.gif" alt="" height="8" width="100%" border="0" vspace="5">
+		<a href="https://dev.eclipse.org/mailman/listinfo/cdt-dev">Mailing list</a><br>
+		<img src="images/subNav_bars.gif" alt="" height="8" width="100%" border="0" vspace="5">
+		<a href="/projects/project_summary.php?projectid=tools.cdt">Project Information</a><br>
+		<img src="images/subNav_bars.gif" alt="" height="8" width="100%" border="0" vspace="5">
+		<a href="https://bugs.eclipse.org/bugs/">Report a bug</a>
+
 	</div>
 </div>
+
+</div>
+</div>
+
 
 EOHTML;
 

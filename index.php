e62361b0a5d042773ff11f955dc899b3648bbb83
dschaefer <dschaefer>
2009-03-05 20:28:12 +0000

    *** empty log message ***

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
 <div id="maincontent">
 
     <div id="RightImage">
-       <img src="images/CDT2.png" border="0"/>
+       <img src="images/CDT2.png" height="80%" width="80%" border="0"/>
     </div>
     <div id="marginLeft">
         <h1>Eclipse C/C++ Development Tooling - CDT</h1>

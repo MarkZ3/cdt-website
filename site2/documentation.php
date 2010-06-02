84a5c427f18b880a59d08e34529c43b2b49dcb33
dschaefer <dschaefer>
2010-06-02 19:58:20 +0000

    Trying out new format.

---
+++ site2/documentation.php
@@ -0,0 +1,86 @@
+<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+/*******************************************************************************
+ * Copyright (c) 2010
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    
+ *******************************************************************************/
+
+	$pageTitle 		= "CDT Documentation";
+
+	$html  = <<<EOHTML
+<div id="midcolumn">
+<h2>$pageTitle</h2>
+<ul>
+<li>
+	<a href="http://www.ibm.com/developerworks/opensource/library/os-eclipse-stlcdt/">
+	Developing applications using the Eclipse C/C++ Development Toolkit </a><br>
+	IBM developerWorks article.
+</li>
+<li>
+	<a href="http://wascana.sourceforge.net/">
+	Wascana Desktop Developer</a><br>
+	CDT based distribution for Windows development.
+</li>
+<li>
+	<a href=" http://www.ibm.com/developerworks/opensource/library/os-ecc/?S_TACT=105AGX44&S_CMP=ART">
+	C/C++ development with the Eclipse Platform</a><br>
+	IBM developerWorks article.
+</li>
+<li>
+	<a href="http://www-128.ibm.com/developerworks/library/os-ecl-vscdt/index.html?ca=dgr-eclipse-1">
+	Migrate Visual Studio C and C++ projects to Eclipse CDT
+	</a><br>
+	A step-by-step guide to moving Microsoft VS projects to Eclipse C/C++ Development Toolkit.
+</li>
+<li>
+	<a href="http://forge.mysql.com/wiki/Eclipse/CDT_on_Linux_and_Mac_OS_X">
+	Eclipse/CDT on Linux and Mac OS X </a><br>
+	Covers installation, configuration, and basic debugging of Eclipse/CDT Europa edition using MySQL as source code.
+</li>
+<li>
+	<a href="http://www.codeproject.com/KB/tips/CPP_Dev_eclipse_CDT.aspx">
+	C++ Development using eclipse IDE
+	</a><br>
+	Starters guide.
+</li>
+<li>
+	<a href="http://live.eclipse.org/node/723">
+	Webinar on Reverse Debugging with DSF-GDB</a><br>
+	This webinar introduces the new DSF-GDB debugger integration that is part of CDT 6.0.  It illustrates the recent GDB additions such as Reverse debugging, Multi-Process and Non-stop multi-thread debugging.
+</li>
+<li>
+	<a href="http://live.eclipse.org/node/293">
+	CDT 4.0 Webinar</a><br>
+	This webinar will walk through all of CDT's features from new project creation, code editing, and source navigation, to build and debug with a special focus on what's new in CDT 4.0.
+</li>
+<li>
+    <a href="http://live.eclipse.org/node/197">
+	Developing C/C++ Applications Webinar</a>
+</li>
+<li>
+	<a href="http://cdtdoug.blogspot.com/index.html">
+	Doug on the Eclipse CDT</a><br>
+	All the latest news from the CDT world.
+</li>
+<li>
+	<a href="http://wiki.eclipse.org/CDT/User/FAQ">
+	CDT FAQ</a><br>
+	All things CDT. May be slightly out of date.
+</li>
+<li>
+	<a href="http://www.eclipse.org/cdt/index_old.html">
+	Old CDT Website</a><br>
+	Covers CDT 3.0 and earlier.
+</li>
+</ul>
+<div>
+EOHTML;
+
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
--- site2/download.php
+++ site2/download.php
@@ -1,6 +1,6 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 /*******************************************************************************
- * Copyright (c) 2009 
+ * Copyright (c) 2010
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,25 +10,44 @@
  *    
  *******************************************************************************/
 
-	$pageTitle 		= "MyProject - Download";
-	$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="/default/style.css"/>');
+	$pageTitle 		= "CDT Downloads";
 
 	$html  = <<<EOHTML
 <div id="midcolumn">
 <h2>$pageTitle</h2>
-<p>All downloads are provided under the terms and conditions of the <a href="/legal/epl/notice.php">Eclipse Foundation Software User Agreement</a> unless otherwise specified.</p>
+<p>All downloads are provided under the terms and conditions of the
+	<a href="/legal/epl/notice.php">Eclipse Foundation Software User Agreement</a>
+	unless otherwise specified.</p>
 
-<h3>Galileo - Eclipse 3.5</h3>
-<p><b>Update site:</b> http://download.eclipse.org/myproject/<br />
-<b>ZIP file: </b><a href="/downloads/download.php?file=/myproject/file.zip">file.zip</a> (10 MiB)</p>
+<p>The CDT can either be installed as part of the Eclipse C/C++ IDE packaged zip file or installed
+	into an existing Eclipse using the "Install New Software..." dialog and entering the p2
+	repository URLs listed below</p>
+
+<h3>CDT 6.0.2 for Eclipse Galileo</h3>
+<p><b><a href="http://www.eclipse.org/downloads/packages/eclipse-ide-cc-developers/galileosr2">
+	Eclipse C/C++ IDE Galileo SR2</a></b></p>
+<p><b>p2 repository:</b> http://download.eclipse.org/cdt/releases/galileo</p>
 
 <h3>Helios - Eclipse 3.6 (unreleased)</h3>
 <p><b>Update site:</b> http://download.eclipse.org/myproject/<br />
 <b>ZIP file: </b><a href="/downloads/download.php?file=/myproject/file.zip">file.zip</a> (10 MiB)</p>
 
+<h3>Additional Distributions</h3>
+<p>The CDT may be installed as part of commercial products or from the following open source
+	distributions.</p>
+	
+<h4>Linux</h4>
+<p>Most major Linux distributions include packages for Eclipse and the CDT.
+	Check your package manager for availability.</p>
+
+<h4>Wascana</h4>
+<p>The <a href="http://www.eclipselabs.org/p/wascana">Wascana</a> project maintains a distribution
+	of the Eclipse C/C++ IDE with the MinGW GNU toolchain and libraries for building Windows
+	applications. See their site for more information</p>
 
 </div>
 EOHTML;
+
 	# Generate the web page
 	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
 ?>

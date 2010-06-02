fa275489d681ec62a08da9005dd4d52ea780cb23
dschaefer <dschaefer>
2010-06-02 17:18:50 +0000

    Trying out new format.

--- site2/_projectCommon.php
+++ site2/_projectCommon.php
@@ -11,22 +11,22 @@
 	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
 	# these are optional
 	$Nav->setLinkList(array());
-	$Nav->addNavSeparator("EGit", 	"/egit/");
-	$Nav->addCustomNav("Download", "/egit/download/", "_self", 3);
-	$Nav->addCustomNav("Documentation", "/egit/documentation/", "_self", 3);
-	$Nav->addCustomNav("Support", "/egit/support/", "_self", 3);
-	$Nav->addCustomNav("Getting Involved", "/egit/developers/", "_self", 3);
+	$Nav->addNavSeparator("CDT", 	"/cdt/");
+	$Nav->addCustomNav("Download", "/cdt/download/", "_self", 3);
+	$Nav->addCustomNav("Documentation", "/cdt/documentation/", "_self", 3);
+	$Nav->addCustomNav("Support", "/cdt/support/", "_self", 3);
+	$Nav->addCustomNav("Getting Involved", "/cdt/developers/", "_self", 3);
 	
-	$pageKeywords	= "eclipse, egit, git, vcs";
+	$pageKeywords	= "eclipse, cdt, git, vcs";
 	$pageAuthor		= "Doug Schaefer";
 	$pageTitle 		= "CDT";
 
 	$Menu->setMenuItemList(array());
 	$Menu->addMenuItem("Home", "/cdt/", "_self");
-	$Menu->addMenuItem("Download", "/egit/download/", "_self");
-	$Menu->addMenuItem("Documentation", "/egit/documentation/", "_self");
-	$Menu->addMenuItem("Support", "/egit/support/", "_self");
-	$Menu->addMenuItem("Developers", "/egit/developers/", "_self");
+	$Menu->addMenuItem("Download", "/cdt/download/", "_self");
+	$Menu->addMenuItem("Documentation", "/cdt/documentation/", "_self");
+	$Menu->addMenuItem("Support", "/cdt/support/", "_self");
+	$Menu->addMenuItem("Developers", "/cdt/developers/", "_self");
 	
 	$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="style.css"/>' . "\n\t");
 	$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="style2.css"/>' . "\n\t");
